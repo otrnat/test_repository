cae784a09a091590174b2e94222c8165bff06cb6
Natalia Otreshko <32938592+otrnat@users.noreply.github.com>
2023-03-27 20:27:29 +0300

    add logic of calculation

--- calculator.py
+++ calculator.py
@@ -4,4 +4,23 @@ num1 = int(input())
 print (f"Enter the second number")
 num2 = int(input ())
 
+print ("Enter the operation which you want:")
+operation = input()
+if operation == "+":
+    sum = num1+num2
+    print(sum)
+if operation == "-":
+    minus = num1-num2
+    print(minus)
+if operation == "*":
+    mulipl = num1*num2
+    print(mulipl)
+if operation == "/":
+    delete = num1 / num2
+    print (delete) 
+         
+
+
+
+
 
